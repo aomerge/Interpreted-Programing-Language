76f1221768a1d483fe81ae9c4655984993ec5bcf
Ángel Ortega <codenode.ortega@gmail.com>
2024-04-28 03:01:05 -0600

    Add .pytest_cache/ to .gitignore and update lexer.py and parser_1.py

--- tests/parcer_test.py
+++ tests/parcer_test.py
@@ -6,6 +6,9 @@ from src.ast import Program
 
 class ParcerTest(TestCase):
     def test_parcer_program(self) -> None:
+        """ test_parcer_program:
+        This function is responsible for testing the parcer program 
+        """
         source: str = 'var a = 1;'
         lexer:Lexer =  Lexer(source)
         parcer:Parser = Parser(lexer)
