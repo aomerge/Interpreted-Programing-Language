7ef8473e4a3700fd891414a94f7528ec23dead93
Ángel Ortega <codenode.ortega@gmail.com>
2024-05-01 01:38:09 -0600

    Refactor parser_test.py to improve readability and add additional test cases

--- src/parser_1.py
+++ src/parser_1.py
@@ -1,23 +1,47 @@
 from src.lexer import Lexer
-from typing import Optional
-from src.ast import (Program, Statement, LetStatement, Identifier)
+from typing import (
+    Optional, 
+    List,
+    Callable,
+    Dict           
+)
+from src.ast import (
+    Program, 
+    Statement, 
+    LetStatement, 
+    Identifier, 
+    ReturnStatement, 
+    ExpressionStatement, 
+    Expression
+)
 from src.token_1 import (Token, TokenType)
 
+prefix_parse_fn = Callable[[], Optional[Statement]]
+infix_parse_fn = Callable[[Statement], Optional[Statement]]
+prefix_parse_fns = Dict[TokenType, prefix_parse_fn]
+infix_parse_fns = Dict[TokenType, infix_parse_fn]
+
 class Parser:
+    
     def __init__(self, lexer: Lexer) -> None:
         self.lexer = lexer        
-        self.current_token:Optional[Token]  = None
+        self._current_token:Optional[Token]  = None
         self._peek_token:Optional[Token] = None
-
+        self._errors: List[str] = []
+        self._prefix_parse_fns: prefix_parse_fns = self._register_prefix_fns()
+        self._infix_parse_fns: infix_parse_fns = self._register_infix_fns()
         self._next_token()
         self._next_token()
-
+    
+    @property
+    def errors(self) -> List[str]:
+        return self._errors
     def getProgram(self) -> Program:
         program: Program = Program(statements=[])
 
-        assert self.current_token is not None
+        assert self._current_token is not None
 
-        while self.current_token.type != TokenType.EOF:
+        while self._current_token.type != TokenType.EOF:
             statement = self._parce_statement()
             if statement is not None:
                 program.statements.append(statement)
@@ -26,37 +50,94 @@ class Parser:
         return program
     
     def _next_token(self) -> None:
-        self.current_token = self._peek_token
+        self._current_token = self._peek_token
         self._peek_token = self.lexer.next_token()
 
     def _expected_tokens(self, token_type: TokenType) -> bool:
-        assert self.current_token is not None
+        assert self._current_token is not None
         if self._peek_token.type == token_type:
             self._next_token()
             return True
-        
+        self._peek_error(token_type)
         return False
     
+    def _parse_identifier(self) -> Optional[Statement]:
+        assert self._current_token is not None
+        return Identifier(token=self._current_token, value=self._current_token.literal)
+    
     def _parse_let_statement(self) -> Optional[Statement]:
-        assert self.current_token is not None
+        assert self._current_token is not None
 
-        let_statement = LetStatement(token=self.current_token)
+        let_statement = LetStatement(token=self._current_token)
 
         if not self._expected_tokens(TokenType.IDENT):
             return None
 
-        let_statement.name = Identifier(token=self.current_token, value=self.current_token.literal)
+        let_statement.name = self._parse_identifier()
 
         if not self._expected_tokens(TokenType.ASSIGN):
             return None
         
-        while self.current_token.type != TokenType.SEMICOLON:
+        while self._current_token.type != TokenType.SEMICOLON:
             self._next_token()
         
         return let_statement
-    
-    def _parce_statement(self) -> Optional[Statement]:
-        if self.current_token.type == TokenType.LET:
-            return self._parse_let_statement()
-        else:
+
+    def _parse_Expression(self) -> Optional[Expression]:
+        assert self._current_token is not None
+        try:
+            prefix_parse_fn = self._prefix_parse_fns[self._current_token.type]
+        except KeyError:
             return None
+        left_expression = prefix_parse_fn()
+        return left_expression
+        
+    def _parse_expression_statement(self) -> Optional[ExpressionStatement]:
+        assert self._current_token is not None
+        expression_statement = ExpressionStatement(token=self._current_token)
+        expression_statement.expression = self._parse_Expression()
+
+        assert self._peek_token is not None
+        if self._peek_token.type == TokenType.SEMICOLON:
+            self._next_token()
+
+        return expression_statement
+    
+    def _parese_identifier(self) -> Identifier:
+        assert self._current_token is not None
+        return Identifier(token=self._current_token, value=self._current_token.literal)        
+
+    def _parce_statement(self) -> Optional[Statement]:
+        if self._current_token.type == TokenType.LET:
+            return self._parse_let_statement()
+        elif self._current_token.type == TokenType.RETURN:
+            return self._parse_return_statement()
+        else:
+            return self._parse_expression_statement()
+
+    def _parse_return_statement(self) -> Optional[Statement]:
+        assert self._current_token is not None
+        return_Statement = ReturnStatement(token=self._current_token)
+        self._next_token()
+
+        while self._current_token.type != TokenType.SEMICOLON:
+            self._next_token()
+
+        return return_Statement
+
+    def _peek_error(self, token_type: TokenType) -> None:
+        assert self._peek_token is not None
+        self._errors.append(f'expected next token to be {token_type}, got {self._peek_token.type} instead')
+    
+    def _register_prefix_fns(self) -> prefix_parse_fns:
+        return {
+            TokenType.IDENT: self._parse_identifier,
+            TokenType.LET: self._parse_let_statement,
+            TokenType.RETURN: self._parse_return_statement
+        }
+    
+    def _register_infix_fns(self) -> infix_parse_fns:
+        return {}
+    
+    def __str__(self) -> str:
+        return f'Parser: current_token={self._current_token}, peek_token={self._peek_token}'
--- tests/parcer_test.py
+++ tests/parcer_test.py
@@ -3,16 +3,25 @@ from src.lexer import Lexer
 from src.parser_1 import Parser
 from typing import (    
     List,
-    Union
+    Union,
+    cast,
+    Any,
 )
 from src.ast import (
+    Expression,
+    ExpressionStatement,
     Program,
     Statement,
-    LetStatement
+    LetStatement,
+    ReturnStatement,
+    Identifier
 )
+import logging
 
 
 class ParcerTest(TestCase):
+    logger = logging.getLogger(__name__)
+
     def test_parcer_program(self) -> None:
         """ test_parcer_program:
         This function is responsible for testing the parcer program 
@@ -23,9 +32,10 @@ class ParcerTest(TestCase):
         
         program:Program = parcer.getProgram()
 
+        self.logger.debug(f'Program test_parcer_program: {program}')
+
         self.assertIsNotNone(program)
         self.assertIsInstance(program, Program)
-
     
     def test_parcer_let_statement(self) -> None:
         """ test_parcer_let_statement:
@@ -37,8 +47,7 @@ class ParcerTest(TestCase):
             let foobar = 838383;
         '''
         lexer:Lexer =  Lexer(source)
-        parcer:Parser = Parser(lexer)
-        print(parcer) 
+        parcer:Parser = Parser(lexer)        
         
         program:Program = parcer.getProgram()        
 
@@ -47,3 +56,105 @@ class ParcerTest(TestCase):
         for statement in program.statements:
             self.assertEqual(statement.token_literal(), 'let')
             self.assertIsInstance(statement, LetStatement)
+    
+    def test_name_in_let_statement(self) -> None:
+        """ test_name_in_let_statement:
+        This function is responsible for testing the name in let statement 
+        """
+        source: str = '''
+            let x = 5;
+            let y = 10; 
+            let foobar = 838383;
+        '''
+        lexer:Lexer =  Lexer(source)
+        parcer:Parser = Parser(lexer)
+        program:Program = parcer.getProgram()
+
+        names:List[str] = ['x', 'y', 'foobar']
+        for i, statement in enumerate(program.statements):            
+            self.assertEqual(statement.name.value, names[i])
+    
+    def test_parse_errors(self) -> None:
+        """ test_parse_errors:
+        This function is responsible for testing the parse errors 
+        """
+        source: str = '''
+            let x 5;
+        '''
+        lexer:Lexer =  Lexer(source)
+        parser:Parser = Parser(lexer)
+        program:Program = parser.getProgram()
+
+        self.assertEqual(len(parser.errors), 1)   
+
+    def test_parse_return_statement(self) -> None:
+        """ test_parse_return_statement:
+        This function is responsible for testing the parse return statement 
+        """
+        source: str = '''
+            return 5;
+            return 10;
+            return 838383;
+        '''
+        lexer:Lexer =  Lexer(source)
+        parser:Parser = Parser(lexer)
+        program:Program = parser.getProgram()        
+
+        for statement in program.statements:
+            self.assertEqual(statement.token_literal(), 'return')
+            self.assertIsInstance(statement, ReturnStatement)
+    
+    def test_parse_identifier_expression(self) -> None:
+        """ test_parse_identifier_expression:
+        This function is responsible for testing the parse identifier expression 
+        """
+        source: str = 'foobar;'
+        lexer:Lexer =  Lexer(source)
+        parser:Parser = Parser(lexer)
+        program:Program = parser.getProgram()
+
+        self._test_program_statement(parser, program)        
+
+        self.logger.debug(f'Program out: {program}')
+
+        expression_statement = cast(ExpressionStatement, program.statements[0])
+
+        
+        self._test_literal_expression(expression_statement.expression, "foobar")
+
+    def _test_program_statement(self,parser: Parser, program: Program, expected_statement_Count: int = 1 ) -> None:
+        """ test_program_statement:
+        This function is responsible for testing the program statement 
+        """        
+        if parser.errors:
+            print(parser.errors)
+
+        self.assertEqual(len(parser.errors), 0)
+        
+        self.assertEqual(len(program.statements), expected_statement_Count)
+        self.assertIsInstance(program.statements[0], ExpressionStatement)
+    
+    def _test_literal_expression(self, expression:Expression, value:Any) -> None:
+        """ _test_literal_expression:
+        This function is responsible for testing the literal expression 
+        """
+        value_type: type = type(value)
+        if value_type == str:
+            self._test_identifier_expression(expression, value)
+        elif value_type == int:
+            self._test_identifier_expression(expression, value)
+        elif value_type == bool:
+            self._test_identifier_expression(expression, value)
+        else:
+            self.fail(f'Type of value not handled. Got={value_type}')
+    
+    def _test_identifier_expression(self, expression: Expression, value:str) -> None:
+        """ test_identifier_expression:
+        This function is responsible for testing the identifier expression 
+        """
+        self.assertIsInstance(expression, Identifier)
+
+        expression = cast(Identifier, expression)     
+        self.assertEqual(expression.value, value)
+        self.assertEqual(expression.token_literal(), value)   
+
