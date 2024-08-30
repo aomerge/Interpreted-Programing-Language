7091a8be7252cb7e902b8747cfed7b51248c325d
aomerge <codenode.ortega@gmail.com>
2024-08-30 08:29:38 -0600

    Refactor ASTNode class to include new Infix and Boolean expressions

--- src/ast.py
+++ src/ast.py
@@ -104,3 +104,115 @@ class Prefix(Expression):
 
     def __str__(self) -> str:
         return f'({self.operator}{str(self.right)})'
+    
+class Prefix(Expression):
+
+    def __init__(self,
+                 token: Token,
+                 operator: str,
+                 right: Optional[Expression] = None) -> None:
+        super().__init__(token)
+        self.operator = operator
+        self.right = right
+
+    def __str__(self) -> str:
+        return f'({self.operator}{str(self.right)})'
+
+class Infix(Expression):
+    def __init__(self, token: Token, left: Optional[Expression] = None, operator: str = "", right: Optional[Expression] = None)-> None:
+        super().__init__(token)
+        self.left = left
+        self.operator = operator
+        self.right = right
+
+    def __str__(self) -> str:
+        return f'({str(self.left)} {self.operator} {str(self.right)})'
+    
+class Boolean(Expression):
+
+    def __init__(self,
+                 token: Token,
+                 value: Optional[bool] = None) -> None:
+        super().__init__(token)
+        self.value = value
+
+    def __str__(self) -> str:
+        return self.token_literal()
+    
+class Block(Statement):
+
+    def __init__(self,
+                 token: Token,
+                 statements: List[Statement]) -> None:
+        super().__init__(token)
+        self.statements = statements
+
+    def __str__(self) -> str:
+        out: List[str] = [str(statement) for statement in self.statements]
+
+        return ''.join(out)
+    
+class If(Expression):
+
+    def __init__(self,
+                 token: Token,
+                 condition: Optional[Expression] = None,
+                 consequence: Optional[Block] = None,
+                 alternative: Optional[Block] = None) -> None:
+        super().__init__(token)
+        self.condition = condition
+        self.consequence = consequence
+        self.alternative = alternative
+
+    def __str__(self) -> str:
+        out: str = f'si {str(self.condition)} {str(self.consequence)}'
+
+        if self.alternative:
+            out += f'si_no {str(self.alternative)}'
+
+        return out
+
+class Function(Expression):
+
+    def __init__(self,
+                 token: Token,
+                 parameters: List[Identifier] = [],
+                 body: Optional[Block] = None) -> None:
+        super().__init__(token)
+        self.parameters = parameters
+        self.body = body
+
+    def __str__(self) -> str:
+        param_list: List[str] = [str(parameter) for parameter in self.parameters]
+
+        params: str = ', '.join(param_list)
+
+        return f'{self.token_literal()}({params}) {str(self.body)}'
+    
+class Call(Expression):
+
+    def __init__(self,
+                 token: Token,
+                 function: Expression,
+                 arguments: Optional[List[Expression]] = None) -> None:
+        super().__init__(token)
+        self.function = function
+        self.arguments = arguments
+
+    def __str__(self) -> str:
+        assert self.arguments is not None
+        arg_list: List[str] = [str(argument) for argument in self.arguments]
+        args: str = ', '.join(arg_list)
+
+        return f'{str(self.function)}({args})'
+    
+class StringLiteral(Expression):
+
+    def __init__(self,
+                 token: Token,
+                 value: str) -> None:
+        super().__init__(token)
+        self.value = value
+
+    def __str__(self) -> str:
+        return super().__str__()
