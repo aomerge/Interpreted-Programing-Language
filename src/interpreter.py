b9d535f476ea944f50fd0f07cccea42ad15ca4a8
aomerge <codenode.ortega@gmail.com>
2024-08-31 12:53:47 -0600

    Refactor interpreter.py to use AST module and improve code organization

--- src/interpreter.py
+++ src/interpreter.py
@@ -1,49 +1,93 @@
-from src.ast import Program, LetStatement, ReturnStatement, ExpressionStatement, Identifier, Integer, Prefix, Infix
+import src.ast as ast
 from src.environment import Environment
+from typing import (
+    Any,
+    cast,
+    List,
+    Optional,
+    Type
+)
+
+from src.object import(
+    Boolean,
+    Null,
+    Object,
+    Integer
+)
 
 class Interpreter:
+
+    TRUE = Boolean(True)
+    FALSE = Boolean(False)
+    NULL = Null()
+
+    _NOT_A_FUNCTION = 'No es una funcion: {}'
+    _TYPE_MISMATCH = 'Discrepancia de tipos: {} {} {}'
+    _UNKNOWN_PREFIX_OPERATOR = 'Operador desconocido: {}{}'
+    _UNKNOWN_INFIX_OPERATOR = 'Operador desconocido: {} {} {}'
+    _UNKNOWN_IDENTIFIER = 'Identificador no encontrado: {}'
+
     def __init__(self):
         self.environment = Environment()
 
-    def interpret(self, program: Program):
-        result = None
-        for statement in program.statements:
-            result = self.evaluate(statement)
-        return result
+    def interpret(self, program: ast.Program)->Optional[Object]:
+        env = self.environment
+        return self._eval_Program(program, env)
 
-    def evaluate(self, node):
-        node_type = type(node).__name__
-        method_name = f'eval_{node_type}'
-        method = getattr(self, method_name, self.no_method_found)
-        return method(node)
+    def _evaluate(self, node: ast.ASTNode , env: Environment)->Optional[Object]:
+        node_type: Type = type(node).__name__
+        
+        if node_type == ast.Program.__name__:
+            return self._eval_Program(cast(ast.Program, node), self.environment)
+        elif node_type == ast.ExpressionStatement.__name__:
+            node = cast(ast.ExpressionStatement, node)
+            assert node.expression is not None
+            return self._evaluate(node.expression, env)
+        elif node_type == ast.Integer.__name__:
+            node = cast(ast.Integer, node)
+            assert node.value is not None
+            return Integer(node.value)
+        elif node_type == ast.Boolean.__name__:
+            node = cast(ast.Boolean, node)
+            assert node.value is not None
+            return self.TRUE if node.value else self.FALSE
+        elif node_type == ast.Prefix.__name__:
+            node = cast(ast.Prefix, node)
+            assert node.right is not None
+            right = self._evaluate(node.right, env)
+            if type(right).__name__ == 'Error':
+                return right
+            return self._eval_PrefixExpression(node.operator, right)
 
     def no_method_found(self, node):
-        raise Exception(f'No se encontró un método de evaluación para el nodo: {type(node).__name__}')
+        return Exception(f'No se encontró un método de evaluación para el nodo: {type(node).__name__}')
 
-    def eval_Program(self, node: Program):
-        result = None
+    def _eval_Program(self, node: ast.Program, env: Environment)->Optional[Object]:
+        result: Optional[Object] = None
+
         for statement in node.statements:
-            result = self.evaluate(statement)
+            result = self._evaluate(statement, env)
+            if type(result).__name__ == 'Return':
+                return result.value
+            if type(result).__name__ == 'Error':
+                return result            
         return result
 
-    def eval_LetStatement(self, node: LetStatement):
+    def eval_LetStatement(self, node: ast.LetStatement):
         value = self.evaluate(node.value)
         self.environment.set(node.name.value, value)
         return value
 
-    def eval_ReturnStatement(self, node: ReturnStatement):
-        return self.evaluate(node.return_value)
+    def eval_ReturnStatement(self, node: ast.ReturnStatement):
+        return self.evaluate(node.return_value)    
 
-    def eval_ExpressionStatement(self, node: ExpressionStatement):
-        return self.evaluate(node.expression)
-
-    def eval_Identifier(self, node: Identifier):
+    def eval_Identifier(self, node: ast.Identifier):
         return self.environment.get(node.value)
 
-    def eval_Integer(self, node: Integer):
+    def eval_Integer(self, node: ast.Integer):
         return node.value
 
-    def eval_Prefix(self, node: Prefix):
+    def _eval_PrefixExpression(self, node: ast.Prefix):
         right = self.evaluate(node.right)
         if node.operator == '-':
             return -right
@@ -52,7 +96,7 @@ class Interpreter:
         else:
             raise Exception(f'Operador prefix no soportado: {node.operator}')
 
-    def eval_Infix(self, node: Infix):
+    def eval_Infix(self, node: ast.Infix):
         left = self.evaluate(node.left)
         right = self.evaluate(node.right)
 
---
+++ src/object.py
@@ -0,0 +1,124 @@
+from enum import Enum, auto
+from abc import ABC, abstractmethod
+from typing import Dict, List
+
+class ObjectType(Enum):
+    BOOLEAN = auto()
+    BUILTIN = auto()
+    ERROR = auto()
+    FUNCTION = auto()
+    NULL= auto()
+    RETURN= auto()
+    STRING= auto()
+    INTEGER= auto()
+
+class Object(ABC):
+    @abstractmethod
+    def type(self) -> ObjectType:
+        pass
+
+    @abstractmethod
+    def inspect(self) -> str:
+        pass
+
+class Integer(Object):
+    def __init__(self, value: int):
+        self.value = value
+
+    def type(self) -> ObjectType:
+        return ObjectType.INTEGER
+
+    def inspect(self) -> str:
+        return str(self.value)
+
+class Boolean(Object):
+    def __init__(self, value: bool):
+        self.value = value
+
+    def type(self) -> ObjectType:
+        return ObjectType.BOOLEAN
+
+    def inspect(self) -> str:
+        return str(self.value).lower()
+
+class Null(Object):
+    def type(self) -> ObjectType:
+        return ObjectType.NULL
+
+    def inspect(self) -> str:
+        return "null"
+    
+class Return(Object):
+    def __init__(self, value: Object):
+        self.value = value
+
+    def type(self) -> ObjectType:
+        return ObjectType.RETURN
+
+    def inspect(self) -> str:
+        return self.value.inspect()
+
+class Error(Object):
+    def __init__(self, message: str):
+        self.message = message
+
+    def type(self) -> ObjectType:
+        return ObjectType.ERROR
+
+    def inspect(self) -> str:
+        return f'Error: {self.message}'
+
+class Environment:
+    def __init__(self):
+        self.store: Dict[str, Object] = {}
+
+    def get(self, name: str) -> Object:
+        obj = self.store.get(name)
+        if obj is None:
+            return Null()
+        return obj
+
+    def set(self, name: str, value: Object):
+        self.store[name] = value
+
+class Function(Object):
+    def __init__(self, parameters: List[str], body: str, env: Environment):
+        self.parameters = parameters
+        self.body = body
+        self.env = env
+
+    def type(self) -> ObjectType:
+        return ObjectType.FUNCTION
+
+    def inspect(self) -> str:
+        return f'fn({", ".join(self.parameters)}) {self.body}'
+
+class String(Object):
+    def __init__(self, value: str):
+        self.value = value
+
+    def type(self) -> ObjectType:
+        return ObjectType.STRING
+
+    def inspect(self) -> str:
+        return self.value
+    
+class BuiltinFunction:
+    def __init__(self, fn):
+        self.fn = fn
+
+    def __call__(self, *args):
+        return self.fn(*args)
+    
+class Builtin(Object):
+    def __init__(self, fn):
+        self.fn = fn
+
+    def type(self) -> ObjectType:
+        return ObjectType.BUILTIN
+
+    def inspect(self) -> str:
+        return "builtin function"
+
+    def __call__(self, *args):
+        return self.fn(*args)
