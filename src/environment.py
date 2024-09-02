23dd1aa51c254b286a7bdb4200759e3d2e7736e1
aomerge <codenode.ortega@gmail.com>
2024-09-01 18:22:09 -0600

    Refactor Environment class to support outer environments and improve get() method

--- src/environment.py
+++ src/environment.py
@@ -1,15 +1,24 @@
-class Environment:
-    def __init__(self):
-        self.store = {}
+from typing import Optional
 
-    def get(self, name: str):
+from src.object import Object
+
+class Environment:
+    def __init__(self, outer: Optional['Environment'] = None):
+        self.store = {}
+        self.outer = outer
+
+    def get(self, name: str) -> Optional[Object]:
+        """Get the value associated with the name in the current environment or outer environments."""
         if name in self.store:
             return self.store[name]
+        elif self.outer:
+            return self.outer.get(name)
         else:
-            raise NameError(f"Variable '{name}' no está definida.")
+            return None
 
-    def set(self, name: str, value):
+    def set(self, name: str, value: Object) -> Object:
+        """Set the value for the name in the current environment."""
         self.store[name] = value
+        print(f"Set {name} to {value}")
+        return value
 
-    def __repr__(self) -> str:
-        return f"Environment({self.store})"
